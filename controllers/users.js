24eeaead956a2ce62ea46a2ab261f475b13b1aec
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-06-02 16:41:51 -0300

    :sparkles: feat: Profile endpoint

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
+const middleware = require('../utils/middleware')
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
@@ -11,6 +12,23 @@ usersRouter.get('/:id', async (request, response) => {
   const user = await User.findById(request.params.id).populate('blogs')
   response.json(user)
 })
+
+usersRouter.get(
+  '/profile/:username',
+  middleware.userExtractor,
+  middleware.requireAuth,
+  async (request, response) => {
+    const { username } = request.params
+
+    if (!username) {
+      return response.status(400).json({ error: '`username` is required' })
+    }
+
+    const user = await User.findOne({ username }).populate('blogs')
+
+    response.json(user)
+  }
+)
 
 usersRouter.post('/', async (request, response) => {
   const { name, username, password } = request.body
